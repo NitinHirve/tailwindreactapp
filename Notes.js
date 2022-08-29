6ab737a89f306a54db523c8e65453c4326de3ef4
NitinHirve <hirvenitin@gmail.com>
2022-08-30 05:15:20 +0530

    first commit

---
+++ Notes.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * Tailwind CSS :
+ *  
+ * 1st step : npm install -D tailwindcss postcss autoprefixer
+ * 2nd step : npx tailwindcss init -p
+ * 
+ * // Icons :
+ * -  npm i @heroicons/react
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
